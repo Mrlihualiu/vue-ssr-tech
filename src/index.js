45bf52a8f57979a8ee0a049022d3277fc1f2e556
liulihua <hc_liulihua@163.com>
2020-05-07 20:16:30 +0800

    init

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
-import Vue from 'vue'
-import App from './app.vue'
-// import './assets/style/demo.styl'
+import Vue from 'vue';
+import App from './app.vue';
 
-const root = document.createElement('div')
-document.body.appendChild(root)
+//import './assets/styles/test.css';
+import './assets/styles/style.styl'
+import './assets/styles/global.styl';
+
+const root = document.createElement('div');
+document.body.appendChild(root);
 
 new Vue({
   render: (h) => h(App)
--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,22 @@
-const path = require('path')
-const webpack = require('webpack')
-const HTMLPlugin = require('html-webpack-plugin')
-const VueLoaderPlugin = require('vue-loader/lib/plugin')
+const path = require('path');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const HTMLPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
-const isDev = process.env.NODE_ENV === 'development' 
+const isDev = process.env.NODE_ENV === 'development';
+
+const webpack = require('webpack');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 const config = {
-  target: 'web',
+  //入口， __dirname 是当前文件所在目录
   entry: path.join(__dirname, 'src/index.js'),
+  //输出
   output: {
-    filename: 'bundle.js',
+    filename: 'bundle.[hash:8].js',
     path: path.join(__dirname, 'dist')
   },
-  mode: 'development',
+  //webpack原生只支持js文件类型，只支持ES5语法，我们使用以.vue文件名结尾的文件时，需要为其指定loader
   module: {
     rules: [
       {
@@ -20,53 +24,107 @@ const config = {
         loader: 'vue-loader'
       },
       {
-        test: /\.js$/,
+        test: /\.jsx$/,
         loader: 'babel-loader'
       },
+      //将小于1024d的图片转为base64，减少http请求
       {
-        test: /\.css$/,
+        test: /\.(gif|jpg|jpeg|png|svg)$/,
         use: [
-          'style-loader',
-          'css-loader'
-        ]
-      },
-      {
-        test: /\.styl/,
-        use: [
-          'style-loader',
-          'css-loader',
-          'stylus-loader'
-        ]
-      },
-      {
-        test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
-        loader: 'url-loader',
-        options: {
-          limit: 10000,
-          name: '[name].[ext]'
-        }
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 1024,
+              name: '[name].[ext]',
+              outputPath: 'assets/img/'
+            }
+          }
+          ]
       }
     ]
   },
   plugins: [
+    new CleanWebpackPlugin(),
+    // 请确保引入这个插件！
     new VueLoaderPlugin(),
     new HTMLPlugin(),
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: isDev ? '"development"' : '"production"'
-      }
-    })
-  ]
-}
+    //new webpack.HotModuleReplacementPlugin(),
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // all options are optional
+      filename: '[name].css',
+      chunkFilename: '[id].css',
+      ignoreOrder: false, // Enable to remove warnings about conflicting order
+    }),
+  ],
 
-if (isDev) {
-  config.devServer = {
-    port: 8000,
-    host: '0.0.0.0',
-    overlay: {
-      errors: true,
+  optimization: {
+    splitChunks: {
+      chunks (chunk) {
+        // exclude `my-excluded-chunk`
+        return chunk.name !== 'my-excluded-chunk';
+      }
     }
   }
 }
 
-module.exports = config
+if(isDev) {
+  config.module.rules.push({
+    //css预处理器，使用模块化的方式写css代码
+    //stylus-loader专门用来处理stylus文件，处理完成后变成css文件，交给css-loader.webpack的loader就是这样一级一级向上传递，每一层loader只处理自己关心的部分
+    test: /\.styl/,
+    use: [
+      'vue-style-loader',
+      'css-loader',
+      { 
+        loader: 'postcss-loader', 
+        options: { sourceMap: true } 
+      },
+      'stylus-loader'
+    ]  
+  });
+  config.devServer = {
+    overlay: {
+      errors: true
+    },
+    hot: true
+  }
+} else {
+  config.output.filename = '[name].[chunkhash:8].js';
+  config.module.rules.push(
+    //css预处理器，使用模块化的方式写css代码
+      //stylus-loader专门用来处理stylus文件，处理完成后变成css文件，交给css-loader.webpack的loader就是这样一级一级向上传递，每一层loader只处理自己关心的部分
+      {
+        test: /\.styl/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              // you can specify a publicPath here
+              // by default it uses publicPath in webpackOptions.output
+              publicPath: './',
+              hmr: process.env.NODE_ENV === 'development',
+            },
+          },
+          'css-loader',
+          { 
+            loader: 'postcss-loader', 
+            options: { sourceMap: true } 
+          },
+          'stylus-loader'
+        ]
+      },
+  );
+
+  config.plugins.push(
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // all options are optional
+      filename: 'styles.[chunkhash].[name].css',
+      chunkFilename: '[id].css',
+      ignoreOrder: false, // Enable to remove warnings about conflicting order
+    })
+  );
+}
+
+module.exports = config;
