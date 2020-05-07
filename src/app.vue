be85d05ef2ea07f841674966841740ed58d6bd04
liulihua <hc_liulihua@163.com>
2020-05-07 23:28:07 +0800

    test

--- src/app.vue
+++ src/app.vue
@@ -28,12 +28,12 @@ export default {
     top 0
     bottom 0
     #cover
-        position absolute
-        left 0
-        right 0
-        top 0
-        bottom 0   
-        background-color #999 
-        opacity 0.2
-        z-index -1
+      position absolute
+      left 0
+      right 0
+      top 0
+      bottom 0   
+      background-color #999 
+      opacity 0.2
+      z-index -1
 </style>
--- src/todo/todo.vue
+++ src/todo/todo.vue
@@ -1,100 +1,61 @@
 <template>
-    <section class="real-app">
-        <input 
-            type="text" 
-            class="add-input"
-            autofocus="autofocus"
-            placeholder="接下来做什么"
-            @keyup.enter="addTodo"
-        >
-        <Item  
-            v-for="todo in filterTodos"
-            :todo="todo"
-            :key="todo.id"
-            @del="deleteTodo"
-        />
-        <Tabs 
-            :filter="filter" 
-            :todos="todos"
-            @toggle="toggleFilter"
-            @clearAll="clearAllCompletedTodo"
-        />
-    </section>
+  <section class="real-app">
+    <input
+      type="text"
+      class="add-input"
+      autofocus="autofocus"
+      placeholder="接下来要做什么"
+      @keyup.enter="addTodo"
+    >
+    <item :todo="todo"></item>
+  </section>
 </template>
 
 <script>
-import Item from './item.vue';
-import Tabs from './tabs.vue';
-import { constants } from 'crypto';
-
-let id = 0;
-
+import Item from './item'
 export default {
-    data() {
-        return {
-            todos: [],
-            filter: 'all'
-        }
-    },
-    components: {
-        Item,
-        Tabs
-    },
-    computed: {
-        filterTodos() {
-            if(this.filter === 'all') {
-                return this.todos;
-            }
-            const filterCompleted = this.filter === 'completed';
-            return this.todos.filter(todo=> todo.completed===filterCompleted);
-        }
-    },
-    methods: {
-        addTodo(e) {
-            this.todos.unshift({
-                id: id++,
-                content: e.target.value,
-                completed: false
-            });
-
-            e.target.value = '';
-        },
-        deleteTodo(id) {
-            this.todos.splice(this.todos.findIndex(todo => id === todo.id), 1);
-        },
-        toggleFilter(state) {
-            console.log(state);
-            this.filter = state;
-        },
-        clearAllCompletedTodo() {
-            this.todos = this.todos.filter(todo=> todo.completed===false);
-        }
+  data() {
+    return {
+      todo: {
+        id: 0,
+        content: 'this is todo',
+        completed: false,
+      }
     }
+  },
+  components: {
+    Item
+  },
+  methods: {
+    addTodo() {
+
+    }
+  }
 }
 </script>
 
 <style lang="stylus" scoped>
 .real-app
-    width 600px
-    margin 0 auto
-    box-shadow 0 0 5px #666
+  width 600px
+  margin 0 auto
+  box-shadow 0 0 5px #666
 
 .add-input
-    position relative
-    margin 0
-    width 100%
-    font-size 24px
-    font-family inherit
-    font-weight inherit 
-    line-height 1.4em
-    border none
-    outline none 
-    color inherit 
-    box-sizing border-box
-    font-smoothing antialiased
-    padding 16px 16px 16px 36px
-    border none
-    box-shadow inset 0 -2px 1px rgba(0, 0, 0, 0.03)
+  position relative
+  margin 0
+  width 100%
+  font-size 24px
+  font-family inherit
+  font-weight inherit 
+  line-height 1.4em
+  border none
+  outline none 
+  color inherit 
+  box-sizing border-box
+  font-smoothing antialiased
+  padding 16px 16px 16px 36px
+  border none
+  box-shadow inset 0 -2px 1px rgba(0, 0, 0, 0.03)
 </style>
 
 
