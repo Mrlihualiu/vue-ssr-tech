c24c9fa9f1699f3cb792c4ac68f119437ed17b7f
mrlihualiu <hc_liulihua@163.com>
2020-12-07 14:04:38 +0800

    xx

--- server/routers/dev-ssr-no-bundle.js
+++ server/routers/dev-ssr-no-bundle.js
@@ -2,36 +2,32 @@ const Router = require('koa-router')
 const axios = require('axios')
 const path = require('path')
 const fs = require('fs')
-const MemoryFS = require('memory-fs')
 const webpack = require('webpack')
 const VueServerRenderer = require('vue-server-renderer')
-
-const serverRender = require('./server-render')
+const serverRender = require('./server-render-no-bundle')
 const serverConfig = require('../../build/webpack.config.server')
 
 const serverCompiler = webpack(serverConfig)
-const mfs = new MemoryFS()
-serverCompiler.outputFileSystem = mfs
 
 let bundle
-serverCompiler.watch({}, (err,stats) => {
+serverCompiler.watch({}, (err, stats) => {
   if (err) throw err
   stats = stats.toJson()
-  stats.errors.forEach(err => console.log(err))
-  stats.warnings.forEach(warn => console.warn(warn))
+  stats.hasErrors.forEach(err => console.log(err))
+  stats.warnings.forEach(warn => console.log(warn))
 
   const bundlePath = path.join(
     serverConfig.output.path,
-    'vue-ssr-server.bundle.json'
+    'server-entry.js'
   )
-
-  bundle = JSON.parse(mfs.readFileSync(bundlePath, 'utf-8'))
+  delete require.cache[bundlePath]
+  bundle = require('../../server-build/server-entry.js').default
   console.log('new bundle generated')
 })
 
 const handleSSR = async (ctx) => {
   if (!bundle) {
-    ctx.body= '等一会，别着急.....'
+    ctx.body = '你等一会，别着急......'
     return
   }
 
@@ -39,22 +35,23 @@ const handleSSR = async (ctx) => {
     'http://127.0.0.1:8000/public/vue-ssr-client-manifest.json'
   )
   const clientManifest = clientManifestResp.data
-  
+
   const template = fs.readFileSync(
     path.join(__dirname, '../server.template.ejs'),
     'utf-8'
   )
 
   const renderer = VueServerRenderer
-    .createBundleRenderer(bundle, {
+    .createRenderer({
       inject: false,
       clientManifest
     })
 
-  await serverRender(ctx, renderer, template)
+  await serverRender(ctx, renderer, template, bundle)
 }
 
 const router = new Router()
-router.get('/*', handleSSR)
+router.get('*', handleSSR)
 
 module.exports = router
+
--- server/server.js
+++ server/server.js
@@ -1,8 +1,8 @@
 const Koa = require('koa')
-const send = require('koa-send')
-const path = require('path')
-const koaBody = require('koa-body')
-const koaSession = require('koa-session')
+// const send = require('koa-send')
+// const path = require('path')
+// const koaBody = require('koa-body')
+// const koaSession = require('koa-session')
 
 // const staticRouter = require('./routers/static')
 // const apiRouter = require('./routers/api')
@@ -55,15 +55,15 @@ app.use(async (ctx, next) => {
 // app.use(staticRouter.routes()).use(staticRouter.allowedMethods())
 // app.use(apiRouter.routes()).use(apiRouter.allowedMethods())
 
-let pageRouter
-if (isDev) {
-  pageRouter = require('./routers/dev-ssr')
-  // pageRouter = require('./routers/dev-ssr-no-bundle')
-} else {
-  pageRouter = require('./routers/dev-ssr')
-  // pageRouter = require('./routers/ssr')
-  // pageRouter = require('./routers/ssr-no-bundle')
-}
+let pageRouter = require('./routers/dev-ssr.js')
+// if (isDev) {
+//   pageRouter = require('./routers/dev-ssr')
+//   // pageRouter = require('./routers/dev-ssr-no-bundle')
+// } else {
+//   pageRouter = require('./routers/dev-ssr')
+//   // pageRouter = require('./routers/ssr')
+//   // pageRouter = require('./routers/ssr-no-bundle')
+// }
 app.use(pageRouter.routes()).use(pageRouter.allowedMethods())
 
 // const HOST = process.env.HOST || '0.0.0.0'
